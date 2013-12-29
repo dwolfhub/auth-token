9e9a9050f18efd2f41537bb2bfe976f817d38121
Daniel Wolf <danielrwolf5@gmail.com>
2013-12-29 16:16:44 -0600

    added auth token filter

--- src/filters.php
+++ src/filters.php
@@ -3,9 +3,17 @@
 
 Route::filter('auth-token', function()
 {
-    if (!isset(Request::server('X-AUTH-KEY'))) {
+    // Check if auth header was sent
+    if (!Request::header('X-AUTH-KEY')) {
         return Response::make('Unauthorized', 401);
     }
 
+    // Check if uuid is valid
+    $user = DB::table('users')->where('uuid', Request::header('X-AUTH-KEY'))->first();
+    if (!$user) {
+        return Response::make('Unauthorized', 401);
+    }
 
+    // Store user in session for later (assumed array sessions)
+    Session::put('user', $user);
 });
