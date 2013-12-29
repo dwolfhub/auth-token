6afdcf0ecbfb575324cc56c735cd5f40b7084e30
Daniel Wolf <danielrwolf5@gmail.com>
2013-12-29 14:12:45 -0600

    initial commit, basic bundle setup

---
+++ src/Dwolf555/AuthToken/AuthTokenServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Dwolf555\AuthToken;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthTokenServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('dwolf555/auth-token');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
